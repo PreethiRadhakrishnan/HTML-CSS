3c8859bafe39f0634f2792313fb8b79df803430a
PreethiRadhakrishnan <preity_preethi@yahoo.com>
2016-05-03 14:09:05 +0530

    js commit

---
+++ javaScript/1.js
@@ -0,0 +1,20 @@
+// alert("Hello , World!");
+// alert("Hello,JavaScript")
+
+var myvariable="Hello,World!";
+alert(myvariable);
+
+var foo= doSomething(2);
+var bar= doSomething(3);
+
+function doSomething(paramOne)
+{
+  paramOne=paramOne+3;
+  paramOne=paramOne+1;
+  paramOne=paramOne*8;
+
+  return paramOne;
+}
+
+alert(foo);
+alert(bar);
